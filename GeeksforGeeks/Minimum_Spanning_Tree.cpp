5301fd9a9fafc951238b9c948a65510370688b33
aryangupta9680 <aryan.gupta9680@gmail.com>
2026-02-01 23:26:37 +0530

    Add second method using Kruskal's algorithm

--- GeeksforGeeks/Minimum_Spanning_Tree.cpp
+++ GeeksforGeeks/Minimum_Spanning_Tree.cpp
@@ -1,3 +1,4 @@
+// First Method: // Using Prim's Algorithm
 class Solution {
   public:
     int spanningTree(int V, vector<vector<int>>& edges) {
@@ -42,6 +43,105 @@ class Solution {
                         pq.push({nextWt, {nextNode, node}});
                     }
                 }
+            }
+        }
+        
+        return cost;
+    }
+};
+
+
+
+
+
+
+
+// Second Method: Using Kruskal's Algorithm
+class Solution {
+  public:
+    int FindParent(int u, vector<int>&parent)
+    {
+        if(u==parent[u])
+        {
+            return u;
+        }
+        
+        return parent[u] = FindParent(parent[u], parent); 
+        // path compression
+    }
+    
+    void UnionByRank(int u, int v, vector<int>&parent, vector<int>&rank)
+    {
+        int pu = FindParent(u, parent);
+        int pv = FindParent(v, parent);
+        
+        if(rank[pu] > rank[pv])
+        {
+            parent[pv] = pu;
+        }
+        else if(rank[pv] > rank[pu])
+        {
+            parent[pu] = pv;
+        }
+        else
+        {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+  
+    int spanningTree(int V, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(V);
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+        
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+        vector<int>parent(V); // ultimate parent will help us to determine set
+        vector<int>rank(V, 0); // it will help us to merge set
+        
+        for(int i = 0; i < V;i++)
+        {
+            parent[i] = i;
+        }
+        
+        vector<pair<int, pair<int, int>>>temp;
+        for(int i = 0; i < V;i++)
+        {
+            for(int j = 0; j < adj[i].size();j++)
+            {
+                temp.push_back({adj[i][j].second, {i, adj[i][j].first}});
+            }
+        }
+        
+        // priority queue
+        // weight, u, v
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>> >, greater<pair<int, pair<int, int>>>> pq(temp.begin(), temp.end());
+        
+        int cost = 0;
+        int edge = 0;
+        while(!pq.empty())
+        {
+            int wt = pq.top().first;
+            int u = pq.top().second.first;
+            int v = pq.top().second.second;
+            pq.pop();
+            
+            // check if they are in different set
+            if(FindParent(u, parent) != FindParent(v, parent))
+            {
+                cost += wt;
+                UnionByRank(u, v, parent, rank);
+                edge++;
+            }
+            
+            if(edge==V-1)
+            {
+                break;
             }
         }
         
