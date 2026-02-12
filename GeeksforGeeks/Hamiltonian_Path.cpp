5cb00fa95f540825e50654abdc2b5798de763090
aryangupta9680 <aryan.gupta9680@gmail.com>
2026-02-12 07:03:37 +0530

    Add solution for Hamiltonian Path problem

---
+++ GeeksforGeeks/Hamiltonian_Path.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    bool DFS(int node, vector<vector<int>>&adj, vector<bool>&visited, int &count, int n)
+    {
+        visited[node] = 1;
+        count++;
+        
+        if(count==n)
+        {
+            return 1;
+        }
+        
+        for(int j = 0; j < adj[node].size();j++)
+        {
+            if(!visited[adj[node][j]] && DFS(adj[node][j], adj, visited, count, n))
+            {
+                return 1;
+            }
+        }
+        
+        visited[node] = 0;
+        count--;
+        return 0;
+    }
+  
+    bool check(int n, int m, vector<vector<int>> edges) {
+        vector<vector<int>>adj(n);
+        for(int i = 0; i < m;i++)
+        {
+            adj[edges[i][0]-1].push_back(edges[i][1]-1);
+            adj[edges[i][1]-1].push_back(edges[i][0]-1);
+        }
+        
+        vector<bool>visited(n, 0);
+        int count = 0;
+        
+        for(int i = 0; i < n;i++)
+        {
+            if(DFS(i, adj, visited, count, n))
+            {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+};
