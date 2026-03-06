d87d75a20577c0a7888fb090b05726c6be4da355
aryangupta9680 <aryan.gupta9680@gmail.com>
2026-03-06 18:13:28 +0530

    Add solution for Intersection of Two Arrays problem

---
+++ LeetCode/Intersection_of_Two_Arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        set<int>s;
+
+        for(auto x: nums1)
+        {
+            s.insert(x);
+        }
+
+        for(auto x: nums2)
+        {
+            if(s.count(x))
+            {
+                ans.push_back(x);
+                s.erase(x);
+            }
+        }
+
+        return ans;
+    }
+};
