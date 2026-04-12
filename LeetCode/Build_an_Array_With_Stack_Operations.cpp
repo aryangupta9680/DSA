47d2b9770b2d6972ee4ec582f3c9149c734a9ca3
aryangupta9680 <aryan.gupta9680@gmail.com>
2026-04-12 08:54:32 +0530

    Add solution for Build an Array With Stack Operations problem

---
+++ LeetCode/Build_an_Array_With_Stack_Operations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        int index = 0;
+
+        for(int i = 1; i <= n && index < target.size();i++)
+        {
+            ans.push_back("Push");
+
+            if(target[index] == i)
+            {
+                index++;
+            }
+            else 
+            {
+                ans.push_back("Pop");
+            }
+        }
+
+        return ans;
+    }
+};
