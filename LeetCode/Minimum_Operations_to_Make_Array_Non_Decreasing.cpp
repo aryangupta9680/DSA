dfb7d330e52cb6765901da4e792464596b2ca594
aryangupta9680 <aryan.gupta9680@gmail.com>
2026-04-26 10:05:52 +0530

    Add solution for Minimum Operations to Make Array Non Decreasing problem

---
+++ LeetCode/Minimum_Operations_to_Make_Array_Non_Decreasing.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        long long ans = 0;
+        int n = nums.size();
+
+        for(int i = 1; i < n;i++)
+        {
+            if(nums[i-1] > nums[i])
+            {
+                ans += (nums[i-1] - nums[i]);
+            }
+        }
+
+        return ans;
+    }
+};
